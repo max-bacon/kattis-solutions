2b1954d29be3e57c8509b79c6ffb73d348f91b0b
Max Bacon <69339912+max-bacon@users.noreply.github.com>
2022-09-21 23:36:05 -0500

    Create hello.java

---
+++ solutions/java/hello.java
@@ -0,0 +1,10 @@
+// https://open.kattis.com/problems/hello
+// Difficulty: 1.2 (Easy)
+
+public class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
