d7595230ea42b874890c8560c97b147b90047b37
Max Bacon <maxbac11@gmail.com>
2022-09-22 18:13:33 -0500

    Added C and C++ solutions for hello

---
+++ solutions/c/hello.c
@@ -0,0 +1,9 @@
+// https://open.kattis.com/problems/hello
+// Difficulty: 1.2 (Easy)
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!");
+    return 0;
+}
---
+++ solutions/cpp/hello.cpp
@@ -0,0 +1,9 @@
+// https://open.kattis.com/problems/hello
+// Difficulty: 1.2 (Easy)
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!";
+    return 0;
+}
