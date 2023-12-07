346be8aec747bb21ad27fa352db85604290ecd03
Max Bacon <maxbac11@gmail.com>
2023-12-07 00:57:24 -0600

    Bus py

---
+++ solutions/python/bus.py
@@ -0,0 +1,9 @@
+# https://open.kattis.com/problems/bus
+# Difficulty: 1.7 (Easy)
+
+or i in range(int(input())):
+    stops = int(input())
+    passengers = 0
+    for j in range(stops):
+        passengers += 2**(j)
+    print(passengers)
